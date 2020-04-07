f80a9351a2cd5dd27d4057b1fdb27841bb86b1a3
IvanGit911 <wang.han.ivan@hotmail.com>
2020-04-06 23:56:16 -0400

    finish dropdown-userinfo

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -8,26 +8,23 @@ class NavBar extends React.Component {
     this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClickOutside(e){
-
-  }
+  handleClickOutside(e) {}
 
   handleClick(e) {
-      document.getElementById("user-dropdown").classList.toggle("show");
+    document.getElementById("user-dropdown").classList.toggle("show");
 
     // if (e.target.matches('dropdown-btn')){
-         
+
     // } else {dropdown.classList.contains('show') && !e.target.matches('dropdown-content')
     //   const dropdown = document.getElementsByClassName('dropdown-content')[0];
     //   dropdown.classList.remove('show')
     // }
-    
   }
 
   loggedout() {
     const { openModal } = this.props;
     return (
-      <div className='session-btn'>
+      <div className="session-btn">
         <button id="login-btn" onClick={() => openModal("login")}>
           Sign in
         </button>
@@ -43,15 +40,20 @@ class NavBar extends React.Component {
     const email = currentUser.email ? currentUser.email.split("@")[0] : null;
     return (
       <div>
-        {/* <div>{currentUser.username[0].toUpperCase()}</div> */}
-        <button onClick={this.handleClick}>{currentUser.username}</button>
+        <button className="currentuser" onClick={this.handleClick}>
+          {currentUser.username[0].toUpperCase()}
+        </button>
         {/* dropdown */}
-        <div>
-          <ul id="user-dropdown" className="dropdown-content">
-            <div>
-              <li>circle</li>
-              <li>{currentUser.username}</li>
-              <li>{`@${email}`}</li>
+        <div id="user-dropdown" className="dropdown-content">
+          <ul className="dropdown-box">
+            <div className="user-info3">
+              <li className="currentuser">
+                {currentUser.username[0].toUpperCase()}
+              </li>
+              <div className="user-info4">
+                <li className="current-username">{currentUser.username}</li>
+                <li>{`@${email}`}</li>
+              </div>
             </div>
             <li>New journal</li>
             <li>My journal</li>
