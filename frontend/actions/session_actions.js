717d210655376efd81fddc5d2b07ba7d9a82fdd6
IvanGit911 <wang.han.ivan@hotmail.com>
2020-04-07 11:11:53 -0400

    fix show modal errs

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -1,17 +1,12 @@
 import * as SessionApiUtil from "../utils/session-api_util";
 import { receiveCurrentUser } from "../actions/user_actions";
+import {receiveErrors} from './error_actions';
 
 export const LOGOUT_CURRENT_USER = "LOGOUT_CURRENT_USER";
 export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
 
 const logoutCurrentUser = () => ({
   type: LOGOUT_CURRENT_USER,
-});
-
-//todo test errors
-export const receiveErrors = (errors = []) => ({
-  type: RECEIVE_SESSION_ERRORS,
-  errors: errors,
 });
 
 
--- frontend/reducers/session_errors_reducer.js
+++ frontend/reducers/session_errors_reducer.js
@@ -1,21 +1,15 @@
-export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
-export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
+import { RECEIVE_ERRORS, CLEAR_ERRORS } from "../actions/error_actions";
 
 const sessionErrorsReducer = (state = [], action) => {
-    Object.freeze(state);
-    switch (action.type) {
-        case RECEIVE_SESSION_ERRORS:
-        return action.errors;
-
-        //?! receive current then clear?
-        case RECEIVE_CURRENT_USER:
-            return [];
-        default:
-            return state;
-    }
+  Object.freeze(state);
+  switch (action.type) {
+    case RECEIVE_ERRORS:
+      return action.errors;
+    case CLEAR_ERRORS:
+      return [];
+    default:
+      return state;
+  }
 };
 
 export default sessionErrorsReducer;
-
-
-
