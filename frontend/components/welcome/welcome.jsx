d602dd5cf76e3915c42b13eac5e86a58f6c07dd6
IvanGit911 <wang.han.ivan@hotmail.com>
2020-04-09 23:50:30 -0400

    finish welcome css

--- frontend/components/welcome/welcome.jsx
+++ frontend/components/welcome/welcome.jsx
@@ -3,22 +3,93 @@ import { connect } from "react-redux";
 
 class Welcome extends React.Component {
   render() {
+    const {openModal} = this.props;
+
     return (
-      <div>
-        <h1>this is the welcome page!!</h1>
-      </div>
+      <ul className="welcome">
+        <h1>We can’t just consume our way to a more sustainable world.</h1>
+        <li>
+          <ul className="checkmark-list">
+            <li className="checkmark">
+              <div>
+                <div className="wel-icon">ic</div>
+                <div>Up-to-date journalisms.</div>
+              </div>
+            </li>
+            <li className="checkmark">
+              <div>
+                <div className="wel-icon">ic</div>
+                <div>Undiscovered trends.</div>
+              </div>
+            </li>
+            <li className="checkmark">
+              <div>
+                <div className="wel-icon">ic</div>
+                <div>Categories you love.</div>
+              </div>
+            </li>
+            <li className="wel-sub-sub-tle">
+              All on MidGreen, all for sustainabiity.
+            </li>
+          </ul>
+        </li>
+        <li>
+          <div className="wel-title">No charge. No ads.</div>
+          <div className="wel-prg">
+            Trust-proof. We don't publish fake journals. Never. Ever.
+          </div>
+          <div className="wel-sub-tle">
+            <div className="sub-tle-btn-box">
+              <div className="wel-btn">{openModal}</div>
+            </div>
+            <div className="sub-tle-msg">
+              <div className="wel-sub-sub-tle">We do things greenly.</div>
+              <div className="wel-prg">
+                MidGreen is not like any other journalism app on the internet.
+                Our sole purpose is to help you find compelling ideas,
+                knowledge, and perspectives. We don’t serve ads—we serve you,
+                the reader who intends to save the planet. MidGreen helps you
+                find the best reading just for you and provides you with
+                unlimited ideas with unlimited journals
+              </div>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div className="wel-tle-2">
+            <h1 className="wel-title">
+              100 million sustainabiity readers and growing.
+            </h1>
+            {/* <div className="wel-background">this need to be fixed</div> */}
+            <div className="wel-cmt">
+              this will be replaced with some awesome comments!!!
+            </div>
+          </div>
+        </li>
+        <li className="wel-btm">
+          <div className="wel-title">Save the planet. Save the legacy.</div>
+        </li>
+        <div className="wel-btn">{openModal}</div>
+        {/* <button className="wel-btn">Get started</button> */}
+      </ul>
     );
   }
 }
 
+import { openModal, closeModal } from "../../actions/modal_action";
 
+const msp = (state) => ({});
 
-const msp = state => ({
-
-})
-
-const mdp = dispatch => ({
-
-})
+const mdp = (dispatch) => ({
+  openModal: (
+    <button
+      onClick={() => {
+        dispatch(openModal("signup"))
+      }}
+    >
+      Get start
+    </button>
+  ),
+});
 
 export default connect(msp, mdp)(Welcome);
