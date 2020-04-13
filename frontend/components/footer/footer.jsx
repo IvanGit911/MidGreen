db849c5250b78866830327f5a5c37ee1d4ba8ce6
IvanGit911 <wang.han.ivan@hotmail.com>
2020-04-13 11:50:41 -0400

    fix footer css

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -3,48 +3,54 @@ import React from "react";
 class Footer extends React.Component {
   render() {
     return (
-      <div className="footer">
-        <h1>ic MidGreen</h1>
+      <footer className="footer">
+        <h1>
+          <img src={window.earth} alt="" />
+          <p>MidGreen</p>{" "}
+        </h1>
         <div className="footer-2">
-          <div>
+          <div className="footer-2-1">
             <h2>
               <a href="https://github.com/IvanGit911/MidGreen">
                 About the site
               </a>
             </h2>
-            <h2>Inspired by</h2>
 
-            <div>
-                ic
-                <a href="">Medium</a>
+            <div className="footer-medium">
+              <h2>Inspired by</h2>
+              <a href="https://medium.com/">
+                <img src={window.medium} alt="" />
+                <img src={window.medium_black} alt="" />
+              </a>
             </div>
-
-            
           </div>
 
           <div className="footer-links">
             <h2>Discover</h2>
-            <a href="">SDG</a>
-            <a href="">Aashe</a>
-            <a href="">...</a>
+            <a href="https://www.epa.gov/">EPA</a>
+            <a href="https://sustainabledevelopment.un.org/sdgs">SDG Goals</a>
+            <a href="https://www.aashe.org/">Aashe</a>
+            <a href="https://www.fema.gov/">FEMA</a>
           </div>
 
-          <div>
+          <div className="footer-info">
             <h2>Contact me</h2>
-            <div className="footer-info">
-              <p>Ivan Wang</p>
-              <p>wang.han.ivan@hotmail.com</p>
-              <p>Tel: 732-875-8961</p>
-            </div>
+
+            <p>Ivan Wang</p>
+            <p>wang.han.ivan@hotmail.com</p>
+            <p>Tel: 732-875-8961</p>
           </div>
-          <ul className="footer-icons">
-            <li>Github</li>
+          <div className="footer-icons">
+            <img src={window.github} alt="" />
+            <img src={window.linkedin} alt="" />
+            <img src={window.angellist} alt="" />
+            {/* <li>Github</li>
             <li>Linkedin</li>
             <li>Anglelist</li>
-            <li>Profolio</li>
-          </ul>
+            <li>Profolio</li> */}
+          </div>
         </div>
-      </div>
+      </footer>
     );
   }
 }
